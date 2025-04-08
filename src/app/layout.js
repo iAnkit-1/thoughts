bef536bda9a7aa0c5e981329076d0a2816b56a8a
Ankit Pushpjivi <ankitsaini5.in@gmail.com>
2025-04-08 21:25:04 +0530

    Project Complete

---
+++ src/app/layout.js
@@ -0,0 +1,32 @@
+"use client";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import { Provider } from "@/context/Provider";
+import { Toaster } from "sonner";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <Provider>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-200`}
+        >
+          <Header />
+          {children}
+          <Toaster position="top-center" />
+        </body>
+      </Provider>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Home from "@/components/Home";
+
+export const metadata = {
+  title: "Express Your Thoughts, Inspire The World.",
+  description:
+    "Share your ideas, opinions, and experience with a liked-minded community.",
+};
+
+const page = () => {
+  return <Home />;
+};
+
+export default page;
